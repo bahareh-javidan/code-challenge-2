494a73439b537f4d958983b97d87c5671fd818f6
Bahar <e.m.javidan@gmail.com>
2024-06-03 16:33:58 +1000

    Making a unidirectional relation

--- src/main/java/com/apromore/challenge/model/Course.java
+++ src/main/java/com/apromore/challenge/model/Course.java
@@ -3,8 +3,6 @@ package com.apromore.challenge.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Set;
-
 @Setter
 @Getter
 @Builder
@@ -19,7 +17,4 @@ public class Course {
     private Long id;
     private String name;
     private String code;
-    @ManyToMany(mappedBy = "courses", cascade = { CascadeType.PERSIST,
-            CascadeType.MERGE, CascadeType.REMOVE })
-    private Set<Student> students;
 }
